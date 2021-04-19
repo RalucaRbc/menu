08cea2780cf03043d88e15a5be679e15a602a677
RalucaRbc <office.movmil@gmail.com>
2021-04-19 12:36:21 +0100

    Project setup

---
+++ src/components/App/App.component.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import Menu from "../Menu/Menu.component";
+import Categories from "../Categories/Categories.component";
+import items from "../../data";
+
+export const App = () => {
+  return <h2>menu project setup</h2>;
+};
---
+++ src/components/Menu/Menu.component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Menu = () => {
+  return <h2>menu component</h2>;
+};
